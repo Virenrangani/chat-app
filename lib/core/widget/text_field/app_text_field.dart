bdf1387f53e816c0483d1362096b48d406ab960e
viren <virenrangani7@gmail.com>
2026-04-08 17:13:17 +0530

    feat : add the custom text form field

---
+++ lib/core/widget/text_field/app_text_field.dart
@@ -0,0 +1,75 @@
+import 'package:flutter/material.dart';
+import '../../constant/border_radius/app_border_radius.dart';
+import '../../constant/colour/app_color.dart';
+import '../../constant/padding/app_padding.dart';
+import '../../constant/text_style/app_text_style.dart';
+
+class AppFormField extends StatelessWidget {
+  final String? hintText;
+  final Widget? prefixIcon;
+  final Widget? suffix;
+  final Widget? prefix;
+  final TextEditingController? controller;
+  final String? Function(String?)? validator;
+  final TextInputType? keyboardType;
+  final Color? borderColor;
+  final bool obscureText;
+  final int maxLines;
+  final VoidCallback? onSuffixTap;
+  final ValueChanged<String>? onChanged;
+
+  const AppFormField({
+    super.key,
+    this.hintText,
+    this.prefixIcon,
+    this.suffix,
+    this.controller,
+    this.validator,
+    this.keyboardType,
+    this.obscureText = false,
+    this.maxLines = 1,
+    this.onSuffixTap,
+    this.onChanged,
+    this.borderColor,
+    this.prefix,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return TextFormField(
+      controller: controller,
+      validator: validator,
+      keyboardType: keyboardType,
+      obscureText: obscureText,
+      obscuringCharacter: "*",
+      maxLines: obscureText ? 1 : maxLines,
+      onChanged: onChanged,
+      style: const TextStyle(fontSize: 16),
+      decoration: InputDecoration(
+        hintText: hintText,
+        hintStyle: AppTextStyles.h2(),
+        prefixIcon: prefixIcon != null
+            ? Padding(
+          padding: AppPadding.edgeAll4,
+          child: prefixIcon,
+        )
+            : null,
+        suffixIcon: suffix != null
+            ? InkWell(
+          onTap: onSuffixTap,
+          child: Padding(padding: AppPadding.edgeAll4, child: suffix),
+        )
+            : null,
+        enabledBorder: OutlineInputBorder(
+          borderRadius: AppBorderRadius.cir16,
+          borderSide: BorderSide(color: AppColor.primary, width: 1.5),
+        ),
+        focusedBorder: OutlineInputBorder(
+          borderRadius: AppBorderRadius.cir16,
+          borderSide: BorderSide(color: AppColor.textSecondary, width: 1.5),
+        ),
+        border: OutlineInputBorder(borderRadius: AppBorderRadius.cir16),
+      ),
+    );
+  }
+}
