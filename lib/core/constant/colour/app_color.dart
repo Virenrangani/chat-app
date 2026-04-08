c22fefe00aa6c5f06c2da047ab96f73e7466ad39
viren <virenrangani7@gmail.com>
2026-04-08 17:12:55 +0530

    feat : add the color class

---
+++ lib/core/constant/colour/app_color.dart
@@ -0,0 +1,35 @@
+import 'dart:ui';
+
+class AppColor {
+
+  static const Color primary = Color(0xFF757575);
+  static const Color primaryLight = Color(0xFF757575);
+  static const Color primaryDark = Color(0xFF757575);
+  static const Color primarySurface = Color(0xFF757575);
+  static const Color primaryContainer = Color(0xFF757575);
+
+  // Secondary Colors
+  static const Color secondary = Color(0xFF757575);
+  static const Color secondaryLight = Color(0xFFFFE082);
+  static const Color secondaryDark = Color(0xFF757575);
+  static const Color secondarySurface= Color(0xFF757575);
+  static const Color secondaryContainer = Color(0xFF087F23);
+
+  // Background Colors
+  static const Color background = Color(0xFFF5F5F5);
+  static const Color backgroundPrimary = Color(0xFFF5F5F5);
+  static const Color scaffoldBackground = Color(0xFFFFFFFF);
+
+  static const Color textPrimary = Color(0xFF212121);
+  static const Color textSecondary = Color(0xFF757575);
+  static const Color textWhite = Color(0xFFFFFFFF);
+
+  // Status Colors
+  static const Color success = Color(0xFF4CAF50);
+  static const Color error = Color(0xFFF44336);
+  static const Color warning = Color(0xFFFF9800);
+  static const Color info = Color(0xFF2196F3);
+
+  static const Color border = Color(0xFFE0E0E0);
+  static const Color divider = Color(0xFFBDBDBD);
+}
