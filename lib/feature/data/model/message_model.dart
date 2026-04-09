932d2f6f711d1c747795271f8425ac0514a8b69d
viren <virenrangani7@gmail.com>
2026-04-09 14:35:56 +0530

    feat : add field to identify the file and load the file

--- lib/feature/data/model/message_model.dart
+++ lib/feature/data/model/message_model.dart
@@ -2,17 +2,23 @@ class MessageModel{
   final String message;
   final String senderId;
   final String timestamp;
+  final String? mediaPath;
+  final String? mediaType;
 
   MessageModel({
     required this.message,
     required this.timestamp,
     required this.senderId,
+    this.mediaPath,
+    this.mediaType,
   });
 
   Map<String, dynamic> toJson() => {
     "message": message,
     "timestamp": timestamp,
     "senderId":senderId,
+    "mediaPath": mediaPath,
+    "mediaType": mediaType,
   };
 
   factory MessageModel.fromJson(Map<String, dynamic> json) {
@@ -20,6 +26,8 @@ class MessageModel{
       message: json["message"],
       timestamp: json["timestamp"],
       senderId: json["senderId"] ?? "",
+      mediaPath: json["mediaPath"],
+      mediaType: json["mediaType"],
     );
   }
 }
