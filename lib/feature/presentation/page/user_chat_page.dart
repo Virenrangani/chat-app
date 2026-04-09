b0e7935c61164de834542435fdc771b04ab27ca6
viren <virenrangani7@gmail.com>
2026-04-09 14:31:13 +0530

    feat : add the all chat and selected file state

--- lib/feature/presentation/page/user_chat_page.dart
+++ lib/feature/presentation/page/user_chat_page.dart
@@ -2,11 +2,13 @@ import 'package:chat_demo/core/constant/padding/app_padding.dart';
 import 'package:chat_demo/core/widget/text_field/app_text_field.dart';
 import 'package:chat_demo/feature/data/data_source/chat_storage.dart';
 import 'package:chat_demo/feature/domain/entities/user.dart';
+import 'package:chat_demo/feature/presentation/widget/chat/audio_chat.dart';
+import 'package:chat_demo/feature/presentation/widget/chat/image_chat.dart';
+import 'package:chat_demo/feature/presentation/widget/chat/video_chat.dart';
 import 'package:chat_demo/feature/presentation/widget/chat_option.dart';
 import 'package:chat_demo/feature/presentation/widget/user_chat.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import '../../../core/constant/border_radius/app_border_radius.dart';
 import '../cubit/chat_cubit.dart';
 import '../cubit/chat_state.dart';
 
@@ -21,13 +23,12 @@ class UserChatPage extends StatefulWidget {
 class _UserChatPageState extends State<UserChatPage> {
   ChatCubit chatCubit = ChatCubit(ChatLocalDataSource());
   TextEditingController messageController = TextEditingController();
-
   late User currentUser;
 
   @override
   void initState() {
     chatCubit.loadMessages();
-    currentUser=widget.user;
+    currentUser = widget.user;
     super.initState();
   }
 
@@ -37,36 +38,67 @@ class _UserChatPageState extends State<UserChatPage> {
       appBar: AppBar(title: Text(currentUser.name)),
       body: BlocBuilder<ChatCubit, ChatState>(
         bloc: chatCubit,
-          builder: (context, state) {
-            if (state is ChatLoaded) {
-              final messages = state.messages;
+        builder: (context, state) {
+          if (state is ChatLoaded) {
+            return Column(
+              children: [
 
-              return UserChat( messages:messages,currentUserId: currentUser.id,);
-            }
-            return SizedBox();
+                Expanded(
+                  child: UserChat(
+                    messages: state.messages,
+                    currentUserId: currentUser.id,
+                  ),
+                ),
+
+                if (state.selectedImage != null)
+                  ImageChat(state:state,chatCubit:chatCubit),
+
+                if (state.selectedAudio != null)
+                  AudioChat(state: state, chatCubit: chatCubit),
+
+                if (state.selectedVideo != null)
+                  VideoChat(state: state, chatCubit: chatCubit),
+
+                Padding(
+                  padding: AppPadding.edgeAll20,
+                  child: AppFormField(
+                    controller: messageController,
+                    prefixIcon: IconButton(
+                      onPressed: () {
+                        showModalBottomSheet(
+                          context: context,
+                          builder: (context) => ChatOption(
+                            onImagePicked: (file) => chatCubit.selectImage(file),
+                            onAudioPicked: (file) => chatCubit.selectAudio(file),
+                            onVideoPicked: (file) => chatCubit.selectVideo(file),
+                          ),
+                        );
+                      },
+                      icon: const Icon(Icons.attach_file),
+                    ),
+                    suffix: IconButton(
+                      onPressed: () {
+                        final text = messageController.text.trim();
+
+                        final hasFile = state.selectedImage != null ||
+                            state.selectedAudio != null ||
+                            state.selectedVideo != null;
+
+                        if (text.isNotEmpty || hasFile) {
+                          chatCubit.sendMessage(text, currentUser.id);
+                          messageController.clear();
+                        }
+                      },
+                      icon: const Icon(Icons.send),
+                    ),
+                  ),
+                ),
+
+              ],
+            );
           }
-      ),
-      bottomNavigationBar: Padding(
-        padding: AppPadding.edgeAll20,
-        child: AppFormField(
-          controller: messageController,
-          prefixIcon: IconButton(
-              onPressed: () {
-                showModalBottomSheet(
-                    context: context,
-                    builder:(context){
-                      return ChatOption();
-                    }
-                );
-              }, icon: Icon(Icons.attach_file)),
-          suffix: IconButton(onPressed: () {
-            final text = messageController.text.trim();
-            if (text.isNotEmpty) {
-              chatCubit.sendMessage(text,currentUser.id);
-              messageController.clear();
-            }
-          }, icon: Icon(Icons.send)),
-        ),
+          return const SizedBox();
+        },
       ),
     );
   }
