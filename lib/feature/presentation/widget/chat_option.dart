aad710040d5823b1310acd87a2969fad5930d176
viren <virenrangani7@gmail.com>
2026-04-09 14:34:02 +0530

    feat : add the pick function to add the chat/file

--- lib/feature/presentation/widget/chat_option.dart
+++ lib/feature/presentation/widget/chat_option.dart
@@ -1,11 +1,22 @@
+import 'dart:io';
 import 'package:chat_demo/core/constant/colour/app_color.dart';
 import 'package:flutter/material.dart';
 import '../../../core/constant/border_radius/app_border_radius.dart';
 import '../../../core/constant/padding/app_padding.dart';
-import '../../../core/util/audio_picker/app_audio_picker.dart';
+import '../../../core/util/app_picker/app_audio_picker.dart';
 
 class ChatOption extends StatelessWidget {
-  const ChatOption({super.key});
+  final Function(File) onImagePicked;
+  final Function(File) onAudioPicked;
+  final Function(File) onVideoPicked;
+
+  const ChatOption({
+    super.key,
+    required this.onImagePicked,
+    required this.onAudioPicked,
+    required this.onVideoPicked,
+  });
+
 
   @override
   Widget build(BuildContext context) {
@@ -15,28 +26,28 @@ class ChatOption extends StatelessWidget {
         borderRadius: AppBorderRadius.verTop12,
         color: AppColor.textWhite,
       ),
-      child: Column(
+      child: Row(
         mainAxisAlignment: MainAxisAlignment.spaceAround,
         children: [
 
           commonIconButton(Icons.image, () {
             Navigator.pop(context);
             Future.delayed(const Duration(milliseconds: 200), () {
-              AppPicker.showImagePicker(context);
+              AppPicker.showImagePicker(context, onPicked: onImagePicked);
             });
           }),
 
           commonIconButton(Icons.audio_file, () {
             Navigator.pop(context);
             Future.delayed(const Duration(milliseconds: 200), () {
-              AppPicker.showAudioPicker(context);
+              AppPicker.showAudioPicker(context, onPicked: onAudioPicked);
             });
           }),
 
           commonIconButton(Icons.videocam_outlined, () {
             Navigator.pop(context);
             Future.delayed(const Duration(milliseconds: 200), () {
-              AppPicker.showVideoPicker(context);
+              AppPicker.showVideoPicker(context, onPicked: onVideoPicked);
             });
           }),
 
