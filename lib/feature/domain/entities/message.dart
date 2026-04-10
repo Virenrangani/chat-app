92dddfc4eab77ceef6276d1bc273fbcac93513ed
viren <virenrangani7@gmail.com>
2026-04-10 09:58:37 +0530

    feat : add message entity in domain layer

--- lib/feature/domain/entities/message.dart
+++ lib/feature/domain/entities/message.dart
@@ -1,3 +1,15 @@
 class Message {
+  final String message;
+  final String senderId;
+  final String timestamp;
+  final String? mediaPath;
+  final String? mediaType;
 
+  const Message({
+  required this.message,
+  required this.senderId,
+  required this.timestamp,
+  this.mediaPath,
+  this.mediaType,
+  });
 }
