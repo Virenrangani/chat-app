e2f614f3c12a1e85402d302c78defe1bdc306d66
viren <virenrangani7@gmail.com>
2026-04-13 17:36:15 +0530

    feat : show the selected video from the device with name

--- lib/feature/presentation/widget/chat/video_chat.dart
+++ lib/feature/presentation/widget/chat/video_chat.dart
@@ -1,26 +1,72 @@
 import 'package:flutter/material.dart';
-
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:video_player/video_player.dart';
 import '../../cubit/chat_cubit.dart';
-import '../../cubit/chat_state.dart';
 
-class VideoChat extends StatelessWidget {
-  final ChatLoaded state;
-  final ChatCubit chatCubit;
+class VideoChat extends StatefulWidget {
 
-  const VideoChat({super.key, required this.state, required this.chatCubit});
+  const VideoChat({super.key});
+
+  @override
+  State<VideoChat> createState() => _VideoChatState();
+}
+
+class _VideoChatState extends State<VideoChat> {
+
+  @override
+  void initState() {
+    super.initState();
+    final file = context.read<ChatCubit>().file;
+    if (file != null) {
+      context.read<ChatCubit>().initVideo(file);
+    }
+  }
+
 
   @override
   Widget build(BuildContext context) {
-    return ListTile(
-      leading: const Icon(Icons.videocam, color: Colors.red),
-      title: Text(
-        state.selectedVideo!.path.split('/').last,
-        overflow: TextOverflow.ellipsis,
-      ),
-      trailing: IconButton(
-        icon: const Icon(Icons.close),
-        onPressed: () => chatCubit.clearVideo(),
-      ),
+    final file = context.watch<ChatCubit>().file;
+    final controller=context.read<ChatCubit>().videoController;
+    if (file == null) return const SizedBox();
+
+    return Stack(
+      children: [
+        Column(
+          children: [
+            controller != null && controller.value.isInitialized
+                ? AspectRatio(
+              aspectRatio: controller.value.aspectRatio,
+              child: VideoPlayer(controller),
+            )
+                : const SizedBox(
+              height: 150,
+              child: Center(child: CircularProgressIndicator()),
+            ),
+            Row(
+              children: [
+                IconButton(
+                  onPressed: (){
+                    context.read<ChatCubit>().toggleVideo();
+                    },
+                  icon: Icon(
+                    context.read<ChatCubit>().isPlaying
+                        ? Icons.pause
+                        : Icons.play_arrow,
+                  ),
+                ),
+                Text(
+                  file.path.split('/').last,
+                  overflow: TextOverflow.ellipsis,
+                ),
+              ],
+            ),
+          ],
+        ),
+        IconButton(
+            icon: const Icon(Icons.close,color: Colors.white,),
+            onPressed: () => context.read<ChatCubit>().clearDocument()
+        ),
+      ],
     );
   }
 }
