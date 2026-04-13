de0992774a66a97056014278835eb47703fda46b
viren <virenrangani7@gmail.com>
2026-04-13 17:35:44 +0530

    feat : show the selected image from the device with name

--- lib/feature/presentation/widget/chat/image_chat.dart
+++ lib/feature/presentation/widget/chat/image_chat.dart
@@ -1,23 +1,23 @@
 import 'package:chat_demo/feature/presentation/cubit/chat_cubit.dart';
 import 'package:chat_demo/feature/presentation/cubit/chat_state.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class ImageChat extends StatelessWidget {
-  final ChatLoaded state;
-  final ChatCubit chatCubit;
-  const ImageChat({super.key,required this.state, required this.chatCubit,});
+  const ImageChat({super.key});
 
   @override
-  Widget build(BuildContext context, ) {
+  Widget build(BuildContext context) {
     return Stack(
       children: [
-        Image.file(state.selectedImage!,
-            height: 180, fit: BoxFit.cover, width: double.infinity),
+        Image.file(
+            context.read<ChatCubit>().file!,
+            height:200, fit: BoxFit.cover, width: double.infinity),
         Positioned(
           top: 4, right: 4,
           child: IconButton(
             icon: const Icon(Icons.close),
-            onPressed: () => chatCubit.clearImage(),
+            onPressed: () => context.read<ChatCubit>().clearDocument()
           ),
         ),
       ],
