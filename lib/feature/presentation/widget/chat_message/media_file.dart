424d7f23f55ce950c588943fa49d3c5761952a29
viren <virenrangani7@gmail.com>
2026-04-13 17:46:29 +0530

    feat : add image , audio and video media file

--- lib/feature/presentation/widget/chat_message/media_file.dart
+++ lib/feature/presentation/widget/chat_message/media_file.dart
@@ -1,33 +1,26 @@
+import 'dart:io';
+import 'package:chat_demo/feature/presentation/widget/media_file/audio_image_file.dart';
+import 'package:chat_demo/feature/presentation/widget/media_file/image_media_file.dart';
+import 'package:chat_demo/feature/presentation/widget/media_file/video_media_file.dart';
 import 'package:flutter/cupertino.dart';
 import '../../../domain/entities/message.dart';
 import '../chat_message.dart';
 
 class MediaFile extends StatelessWidget{
   final Message msg;
-
   const MediaFile({super.key, required this.msg});
 
   @override
   Widget build(BuildContext context) {
-    final MediaType? type = MediaType.fromString(msg.mediaType);
-
-    if (type == null || msg.mediaPath == null) return const SizedBox.shrink();
-
-    return Padding(
-      padding: const EdgeInsets.only(bottom: 6),
-      child: Row(
-        mainAxisSize: MainAxisSize.min,
-        children: [
-          Icon(type.icon, color: type.color),
-          const SizedBox(width: 8),
-          Flexible(
-            child: Text(
-              msg.mediaPath!.split('/').last,
-              overflow: TextOverflow.ellipsis,
-            ),
-          ),
-        ],
-      ),
-    );
+    switch (MediaType.fromString(msg.mediaType)) {
+      case MediaType.image:
+        return ImageMediaFile(file: File(msg.mediaPath?? ""));
+      case MediaType.audio:
+        return AudioImageFile(file: File(msg.mediaPath?? ""),);
+      case MediaType.video:
+        return VideoMediaFile(file: File(msg.mediaPath?? ""),);
+      case null:
+        return const SizedBox.shrink();
+    }
   }
 }
