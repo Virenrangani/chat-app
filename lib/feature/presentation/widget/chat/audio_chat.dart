673f305c382ace072d847d05b7abae936e4f1bf7
viren <virenrangani7@gmail.com>
2026-04-13 17:36:04 +0530

    feat : show the selected audio from the device with name

--- lib/feature/presentation/widget/chat/audio_chat.dart
+++ lib/feature/presentation/widget/chat/audio_chat.dart
@@ -1,24 +1,31 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import '../../cubit/chat_cubit.dart';
-import '../../cubit/chat_state.dart';
 
-class AudioChat extends StatelessWidget {
-  final ChatLoaded state;
-  final ChatCubit chatCubit;
+class AudioChat extends StatefulWidget {
+  const AudioChat({super.key});
 
-  const AudioChat({super.key,required this.state,required this.chatCubit});
+  @override
+  State<AudioChat> createState() => _AudioChatState();
+}
+
+class _AudioChatState extends State<AudioChat> {
 
   @override
   Widget build(BuildContext context) {
+
     return ListTile(
       leading: const Icon(Icons.audio_file, color: Colors.blue),
       title: Text(
-        state.selectedAudio!.path.split('/').last,
+        context.read<ChatCubit>().file!.path.split('/').last,
         overflow: TextOverflow.ellipsis,
       ),
+      subtitle:IconButton(onPressed: (){
+        context.read<ChatCubit>().toggleAudio();
+      }, icon: Icon(context.watch<ChatCubit>().isPlaying?Icons.pause:Icons.play_arrow)),
       trailing: IconButton(
         icon: const Icon(Icons.close),
-        onPressed: () => chatCubit.clearAudio(),
+        onPressed: () => context.read<ChatCubit>().clearDocument()
       ),
     );
   }
