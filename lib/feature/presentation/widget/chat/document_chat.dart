b62256866d730d09fb3d27cbc659132005796a60
viren <virenrangani7@gmail.com>
2026-04-14 15:39:09 +0530

    feat : add document chat user preview the document

---
+++ lib/feature/presentation/widget/chat/document_chat.dart
@@ -0,0 +1,31 @@
+import 'package:chat_demo/core/widget/file_extension_icon/file_extension_icon.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:open_filex/open_filex.dart';
+import '../../cubit/chat_cubit.dart';
+
+class DocumentChat extends StatelessWidget {
+  const DocumentChat({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final file=context.read<ChatCubit>().file;
+    final String? extension=file?.path.split('.').last;
+    if(file == null){
+      return const SizedBox.shrink();
+    }
+    return ListTile(
+      leading: getFileIcon(extension!),
+      title: Text(
+        file.path.split('/').last,
+        overflow: TextOverflow.ellipsis,
+      ),
+      subtitle: const Text("Tap to open"),
+      onTap: () => OpenFilex.open(file.path),
+      trailing: IconButton(
+        icon: const Icon(Icons.close),
+        onPressed: () => context.read<ChatCubit>().clearDocument(),
+      ),
+    );
+  }
+}
