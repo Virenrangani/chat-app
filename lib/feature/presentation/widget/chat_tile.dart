abb48fcac55a2dd4d555c7b6f8cb94d08067c4fb
viren <virenrangani7@gmail.com>
2026-04-17 11:30:51 +0530

    feat : add the inkwell for navigation and circle avatar to show the name first latter

--- lib/feature/presentation/widget/chat_tile.dart
+++ lib/feature/presentation/widget/chat_tile.dart
@@ -1,3 +1,6 @@
+import 'package:chat_demo/core/constant/colour/app_color.dart';
+import 'package:chat_demo/core/constant/padding/app_padding.dart';
+import 'package:chat_demo/core/constant/text_style/app_text_style.dart';
 import 'package:chat_demo/feature/presentation/page/user_chat_page.dart';
 import 'package:flutter/material.dart';
 import '../../../core/util/date/app_date.dart';
@@ -9,15 +12,34 @@ class ChatTile extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ListTile(
-      leading: CircleAvatar(),
-      title: Text(user.name),
-      trailing: Text(DateFormatter.hourMinuteFormat(DateTime.now())),
-      onTap: (){
-        Navigator.push(context,
-            MaterialPageRoute(builder:
-                (context)=>UserChatPage(user: user,)));
+    return InkWell(
+      onTap: () {
+        Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (context) => UserChatPage(user: user),
+          ),
+        );
       },
+      child: Padding(
+        padding: AppPadding.edgeAll12,
+        child: Row(
+          children: [
+
+            CircleAvatar(
+              radius: 26,
+              backgroundColor: AppColor.primaryLight,
+              child: Text(
+                (user.name[0]),
+                style: AppTextStyles.h5(color: AppColor.primary)
+              ),
+            ),
+
+            SizedBox(width: 12),
+
+          ],
+        ),
+      ),
     );
   }
 }
