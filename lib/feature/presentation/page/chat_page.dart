29de5b04bfcd21455d91b38142ecc8d7b260ce8a
viren <virenrangani7@gmail.com>
2026-04-17 10:24:32 +0530

    feat : add chat title class and remove appbar

--- lib/feature/presentation/page/chat_page.dart
+++ lib/feature/presentation/page/chat_page.dart
@@ -1,3 +1,7 @@
+import 'package:chat_demo/core/constant/colour/app_color.dart';
+import 'package:chat_demo/core/constant/padding/app_padding.dart';
+import 'package:chat_demo/core/constant/text_style/app_text_style.dart';
+import 'package:chat_demo/feature/presentation/widget/app_bar/chat_title.dart';
 import 'package:chat_demo/feature/presentation/widget/chat_list.dart';
 import 'package:flutter/material.dart';
 
@@ -7,8 +11,27 @@ class ChatPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("Chat Screen"),),
-      body: Chats()
+      backgroundColor: Colors.black,
+      body: Column(
+        children: [
+          Container(
+            width: double.infinity,
+            decoration: BoxDecoration(
+              color:AppColor.primaryDark
+            ),
+            child: Padding(
+              padding: AppPadding.edgeSymmetricHori16,
+              child: Column(
+                children: [
+                  SizedBox(height:80,),
+                  ChatTitle()
+                ],
+              ),
+            ),
+          ),
+          Expanded(child: Chats()),
+        ],
+      )
     );
   }
 }
